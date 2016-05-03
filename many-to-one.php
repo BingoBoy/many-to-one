8cc699e0c0a535e5d1184d73f3f473c7d9593d5f
Truls Dahl <truls.dahl@me.com>
2016-05-03 17:05:15 +0200

    Update many-to-one.php

--- many-to-one.php
+++ many-to-one.php
@@ -1,4 +1,15 @@
 
 <?php
+/*
+Plugin Name: One to Many
+Plugin URI:  http://tyfon.no/
+Text Domain: tsk_otm
+Domain Path: /languages
+Description: One to Many are a plugin that combines two or more pdf files into one pdf file.
+Author:      Truls Dahl
+Version:     0.1
+Author URI:  http://tyfon.no/
+*/
+
 
 ?>
